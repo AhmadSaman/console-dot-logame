6e59c6a69356ec10f2bf828e46179c1958408a9d
Ahmad Saman Abdulhadi <ahmadsaman.eng@gmail.com>
2023-04-22 16:37:26 +0300

    add: some changes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,5 @@
 import { styled } from "config/stitches.config";
-import { TypeAnimation } from "react-type-animation";
-
-const Div = styled("div", {
-  color: "$color-4",
-});
-const TextAnimation = styled(TypeAnimation, {
-  fontSize: "1.5rem",
-});
 
 export default function Index() {
-  return (
-    <Div>
-      <TextAnimation sequence={["Console.logame", 1000]} speed={50} />
-      <div>
-        <TextAnimation
-          sequence={[2000, "null", 1000, "undefined", 1000, "NaN", 1000]}
-          speed={50}
-        />
-      </div>
-    </Div>
-  );
+  return <div>ahmad</div>;
 }
